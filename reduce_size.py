2cad587071d9742a8fea7e30886766d8a0622a11
Sophia Lilmohan <lilmohan.sophia4@gmail.com>
2023-10-29 17:56:34 -0400

    Take in user input from GUI to reduce layer size

--- reduce_size.py
+++ reduce_size.py
@@ -10,20 +10,17 @@ sys.path.insert(0, mydir)
 
 import item_util
 
-def reduce_size(image, drawable):
+def reduce_size(image, drawable, reduce_factor):
     """
     Reduces every visible layer by some factor (currently by 1.3)
     Additionally will also center layers
     """
-    
-    #TODO: take in this value as a parameter from GUI
-    reduceFactor = 1.30
 
     layers = [] #list of active layers
     try:
         item_util.listAllVisible(image, layers)
         for layer in layers:
-            pdb.gimp_layer_scale(layer, layer.width/reduceFactor, layer.height/reduceFactor, False)
+            pdb.gimp_layer_scale(layer, layer.width/reduce_factor, layer.height/reduce_factor, False)
 
             #center the image
             item_util.center_layer(image, layer)
@@ -35,7 +32,7 @@ def reduce_size(image, drawable):
 
 register(
     "Reduce_Size",
-    "Reduce layers size by .25 ",
+    "Reduce layers size by inputed factor",
     "Reduce layers size",
     "Sophia Lilmohan",
     "Sophia Lilmohan",
@@ -43,7 +40,7 @@ register(
     "<Image>/Functionality/Reduce_Size",
     "*",
     [
-        #(PF_FLOAT, "y_pos", "y_pos:", 0)
+        (PF_FLOAT, "reduce_factor", "reduce_factor:", 1)
     ],
     [],
     reduce_size)
